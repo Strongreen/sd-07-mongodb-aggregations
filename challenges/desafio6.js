4dcb18d916f1546ae64b0dd1f227e277c1ed61d8
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2021-03-20 18:38:20 -0300

    Não existe inicio e nem chegada, só o caminho...

---
+++ challenges/desafio6.js
@@ -0,0 +1,28 @@
+// Regex consultado na página: https://docs.mongodb.com/manual/reference/operator/query/regex/
+db.movies.aggregate([
+  {
+    $match: {
+      awards: {
+        $regex: /^Won.*Oscar/si,
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      maior_rating: { $max: "$imdb.rating" },
+      menor_rating: { $min: "$imdb.rating" },
+      media_rating: { $avg: "$imdb.rating" },
+      desvio_padrao: { $stdDevSamp: "$imdb.rating" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      maior_rating: 1,
+      menor_rating: 1,
+      media_rating: { $round: ["$media_rating", 1] },
+      desvio_padrao: { $round: ["$desvio_padrao", 1] },
+    },
+  },
+]);

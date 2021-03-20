62daf4a8d3825bdcaf68f8719c236a5f5d7d2f6d
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2021-03-20 18:42:51 -0300

    Lint desafio 1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -4,7 +4,7 @@ db.movies.aggregate([
       "imdb.rating": { $gte: 7 },
       genres: { $nin: ["Crime", "Horror"] },
       rated: { $in: ["PG", "G"] },
-      languages: { $all: ["Spanish", "English"]},
-    }
+      languages: { $all: ["Spanish", "English"] },
+    },
   },
 ]);

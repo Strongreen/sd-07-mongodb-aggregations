65ae888f355011ca6c7c8f3c05cd7e07f85edac1
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2021-03-20 17:57:02 -0300

    Desafios 01, 02 e 03

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,3 +1,4 @@
+// The current database to use.
 // use("aggregations");
 
 db.movies.aggregate([
@@ -21,6 +22,23 @@ db.movies.aggregate([
           },
         },
       ],
+    },
+  },
+  {
+    $project: {
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+      _id: 0,
+    },
+  },
+  {
+    $sort: {
+      ano: -1,
+      notaIMDB: -1,
+      titulo: 1,
     },
   },
 ]);

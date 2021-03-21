21e67cf6d129edcdc165627abd4e6b37db90bcbd
Lenodeoliveira <lenodeoliveira@gmail.com>
2021-03-21 17:05:31 -0300

    Adicionando requisito 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,13 +1,32 @@
 db.movies.aggregate([
   {
-    $match: {
-      countries: { $eq: "USA" },
+    $addFields: {
+      favoritos: [
+        "Sandra Bullock",
+        "Tom Hanks",
+        "Julia Roberts",
+        "Kevin Spacey",
+        "George Clooney",
+      ],
     },
   },
   {
-    $project: {
-      _id: 0,
-      title: 1,
+    $addFields: {
+      num_favs: { $size: { $setIntersection: ["$cast", "$favoritos"] } },
     },
   },
+  {
+    $match: {
+      $and: [
+        { countries: { $eq: "USA" } },
+        { "tomatoes.viewer.rating": { $gte: 3 } },
+        { cast: { $exists: true } },
+      ],
+    },
+  },
+
+  { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
+  { $skip: 24 },
+  { $limit: 1 },
+  { $project: { title: 1, _id: 0 } },
 ]);

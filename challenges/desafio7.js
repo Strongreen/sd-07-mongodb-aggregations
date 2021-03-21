73d976f61aa0db346af461d5b0d5926e4d04ad5c
Francisco <franciscoliratorres@gmail.com>
2021-03-21 18:35:39 -0300

    add desafio 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,23 @@
+db.movies.aggregate([
+  {
+    $match: {
+      languages: { $all: ["English"] },
+    },
+  },
+  {
+    $unwind: "$cast",
+  },
+  {
+    $group: {
+      _id: "$cast",
+      numeroFilmes: { $sum: 1 },
+      media: { $avg: "$imdb.rating" },
+    },
+  },
+  {
+    $project: {
+      numeroFilmes: 1,
+      mediaIMDB: { $round: ["$media", 1] },
+    },
+  },
+]);

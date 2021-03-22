f6391cf1296661d26f4b9887843562d9f1709716
marcelo ivan <sagacello@hotmail.com>
2021-03-22 19:14:19 -0300

    10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,23 +1,26 @@
-db.trips.aggregate([
-  {
-    $group: {
-      _id: "$usertype",
-      duracaoMedia: {
-        $avg: {
-          $subtract: ["$stopTime", "$startTime"],
+db.trips.aggregate(
+  [
+    {
+      $group: {
+        _id: "$usertype",
+        duracaoMedia: {
+          $avg: {
+            $subtract: ["$stopTime", "$startTime"],
+          },
         },
       },
     },
-  },
-  {
-    $project: {
-      _id: 0,
-      tipo: "$_id",
-      duracaoMedia: {
-        $round: [{
-          $divide: ["$duracaoMedia", 60 * 60 * 1000],
-        }, 2],
+    {
+      $sort: {
+        duracaoMedia: 1,
       },
     },
-  },
-]);
+    {
+      $project: {
+        _id: false,
+        tipo: "$_id",
+        duracaoMedia: { $round: [{ $divide: ["$duracaoMedia", 3.6e+6] }, 2] },
+      },
+    },
+  ],
+);

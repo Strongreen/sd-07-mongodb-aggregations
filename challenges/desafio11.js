62ac512b0d7febdff1e5203e6cda465a25555eb2
Vitor Rodrigues <vitor_rc@outlook.com>
2021-03-22 18:33:00 -0300

    Requisito 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,15 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      diaDaSemana: { $dayOfWeek: "$startTime" },
+    },
+  },
+  {
+    $group: {
+      _id: "$diaDaSemana",
+      total: { $sum: 1 },
+    },
+  },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+]);

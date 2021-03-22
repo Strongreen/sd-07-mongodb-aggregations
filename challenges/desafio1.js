7882cf70743c517530bbe34a57aa06a324905e59
BrunaSepulveda <sepulvedabrun@gmail.com>
2021-03-22 16:40:14 -0300

    tentativa desafio1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,1 +1,12 @@
-db.movies.aggregate([{$match: {imdb.rating: 7}}, {$group: { _id: "title"}}])
+db.movies.aggregate([
+  {
+    $match: {
+      $and: [
+        {"imdb.rating": 7},
+        { genres: { $nin: ["Crime", "Horror"] } },
+        { rated: {$in:["PG","G"]} },
+        { languages: {$in:["English", "Spanish"]} }
+      ],
+    },
+  }
+]);

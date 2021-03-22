94c6868a7e356af148fdfa140584de5dfdd673e6
marcelo ivan <sagacello@hotmail.com>
2021-03-22 18:56:36 -0300

    7 e 10 com problema

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -17,10 +17,7 @@ db.movies.aggregate(
       },
     },
     {
-      $sort: {
-        _id: -1,
-        numeroFilmes: -1,
-      },
+      $sort: { numeroFilmes: -1, _id: -1 },
     },
     {
       $project: {

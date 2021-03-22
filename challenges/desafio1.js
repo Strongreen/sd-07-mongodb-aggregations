190ac2ceabfc45b8dfb18fb467f5eb12289a4c17
moacyrrln <moacyrrln@gmail.com>
2021-03-22 17:21:52 -0300

    desafio1 correção1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,3 +1,7 @@
-db.movies.aggregate(
-  [ { $match: { "imdb.rating": { $gte: 7 } } } ]
-);
+db.movies.aggregate([{
+  $match: {
+    "imdb.rating": { $gte: 7 },
+    genres: { $elemMatch: { $nin: ["Crime", "Horror"] } },
+    rated: { $in: ["PG", "G"] },
+  },
+}]);

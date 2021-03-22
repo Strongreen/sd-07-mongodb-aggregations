dfbedf96b9530ea15bdb9f21318715b9f15efbf7
marcelo ivan <sagacello@hotmail.com>
2021-03-22 15:01:59 -0300

    erros no 1 2 e 3

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -11,7 +11,7 @@ db.movies.aggregate([
       "imdb.rating": { $gte: 7 },
       genres: { $nin: ["Crime", "Horror"] },
       rated: { $in: ["PG", "G"] },
-      languaages: { $all: ["English", "Spanish"] },
+      languages: { $all: ["English", "Spanish"] },
     },
   },
 ]);
---
+++ challenges/desafio14.js
@@ -0,0 +1,32 @@
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: {
+        $gte: ISODate("2016-03-10T00:00:00Z"),
+        $lt: ISODate("2016-03-11T00:00:00Z"),
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$bikeid",
+      media: {
+        $avg: {
+          $subtract: ["$stopTime", "$startTime"],
+        },
+      },
+    },
+  },
+  {
+    $limit: 5,
+  },
+  {
+    $project: {
+      _id: 0,
+      bikeId: "$_id",
+      duracaoMedia: {
+        $round: [{ $divide: ["$media", 60000] }, 0], // minuto para milessegundos
+      },
+    },
+  },
+]);

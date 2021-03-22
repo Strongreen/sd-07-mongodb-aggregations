5c86d6c374a74c2c01431630b6db3d16661ef9bb
Pedro Marques Fr <pedromarques1011@gmail.com>
2021-03-22 10:42:05 -0300

    hm

---
+++ challenges/desafio9.js
@@ -0,0 +1,19 @@
+db.trips.aggregate([
+  {
+    $match: { birthYear: { $and: [{ $exists: true }, { $not: { $eq: "" } }] } },
+  },
+  {
+    $group: {
+      _id: "",
+      maiorAnoNascimento: { $max: { $toInt: "$birthYear" } },
+      menorAnoNascimento: { $min: { $toInt: "$birthYear" } },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      maiorAnoNascimento: 1,
+      menorAnoNascimento: 1,
+    },
+  },
+]);

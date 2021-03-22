d53e26a6ab5c89d9f5ae5ab2014ac6a420a08608
Strongreen <strongreen@live.com>
2021-03-22 20:03:49 -0300

    desafio 4

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,7 +1,7 @@
 /** @format */
 
-db.movies.aggregate(
+db.movies.aggregate([
   { $addFields: { title_split: { $split: ["$title", " "] } } },
   { $match: { title_split: { $size: 1 } } },
   { $sort: { title: 1 } },
-);
+)];

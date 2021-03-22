87ab3938f5057286c7f6046fe14d574dfc71ed9c
Carol Bezerra <carol.arbe@gmail.com>
2021-03-22 20:07:43 -0300

    Desafio 10 corrigido

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -3,6 +3,8 @@ db.trips.aggregate([
     _id: "$usertype",
     duracaoMedia: { $avg: { $subtract: ["$stopTime", "$startTime"] } } } },
   { $project: {
+    _id: 0,
+    tipo: "$_id",
     duracaoMedia: { $round: [{ $divide: ["$duracaoMedia", 3600000] }, 2] },
   } },
 ]);

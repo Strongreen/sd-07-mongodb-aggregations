f20b5c14c1056ae052249b9bfbb40eb9075cad84
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-03-23 15:21:56 -0300

    desafio 5 skip e limit

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -16,5 +16,6 @@ db.movies.aggregate([
   },
   { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
   { $project: { title: 1, _id: 0, num_favs: 1 } },
-  { $limit: 90 },
+  { $skip: 24 },
+  { $limit: 1 },
 ]);

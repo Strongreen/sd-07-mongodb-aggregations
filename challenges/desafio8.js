a39e4bd6d41b95596051db3469b6261a2dee6937
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-03-23 16:08:59 -0300

    Projeto concluido

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,19 +1,22 @@
-db.air_airlines.aggregate([
-
-  { $lookup: {
-    from: "air_routes",
-    localField: "airline",
-    foreignField: "airline.id",
-    as: "rotas",
-  } },
-]).pretty();
-
-db.air.alliances.aggregate([
+db.air_alliances.aggregate([
   { $unwind: "$airlines" },
   { $lookup: {
-    from: "air_airlines",
+    from: "air_routes",
     localField: "airlines",
-    foreignField: "name",
+    foreignField: "airline.name",
     as: "rotas",
   } },
+  { $unwind: "$rotas" },
+  { $match: {
+    $or: [
+      { "rotas.airplane": { $eq: "747" } },
+      { "rotas.airplane": { $eq: "380" } },
+    ],
+  } },
+  { $group: {
+    _id: "$name",
+    totalRotas: { $sum: 1 },
+  } },
+  { $sort: { totalRotas: -1 } },
+  { $limit: 1 },
 ]);

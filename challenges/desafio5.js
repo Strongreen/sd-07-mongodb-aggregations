59905fb422fa535d6804f671607bd1844e36e846
renatomak <renato.mark.silva@gmail.com>
2021-03-23 14:25:07 -0300

    Desafio 5 - Correçao

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,6 +1,10 @@
 db.movies.aggregate([
   {
     $match: {
+      countries: "USA",
+      "tomatoes.viewer.rating": {
+        $gte: 3,
+      },
       cast: {
         $exists: true,
         $in: [
@@ -35,6 +39,9 @@ db.movies.aggregate([
   },
   {
     $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 },
+  },
+  {
+    $skip: 24,
   },
   {
     $project: {

a9c253378441439144d64d673dda37947056d5f0
Strongreen <strongreen@live.com>
2021-03-23 14:31:40 -0300

    requisito 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,30 @@
+/** @format */
+
+db.movies.aggregate([
+  {
+    $match: {
+      languages: "English",
+      "imdb.rating": { $exists: true },
+    },
+  },
+  { $unwind: "$cast" },
+  {
+    $group: {
+      _id: "$cast",
+      numeroFilmes: { $sum: 1 },
+      mediaIMDB: { $avg: "$imdb.rating" },
+    },
+  },
+  {
+    $sort: {
+      _id: -1,
+      numeroFilmes: -1,
+    },
+  },
+  {
+    $project: {
+      numeroFilmes: 1,
+      mediaIMDB: { $round: ["$mediaIMDB", 1] },
+    },
+  },
+]);

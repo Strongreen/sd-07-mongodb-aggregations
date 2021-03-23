7ffec2b6ffc81fc2bd9934d9ecd2cb0b94a18073
Carol Bezerra <carol.arbe@gmail.com>
2021-03-23 16:43:52 -0300

    Requisito 7

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -2,6 +2,6 @@ db.movies.aggregate([
   { $match: { $and: [{ languages: { $exists: 1 } }, { languages: { $all: ["English"] } }] } },
   { $unwind: "$cast" },
   { $group: { _id: "$cast", numeroFilmes: { $sum: 1 }, mediaIMDB: { $avg: "$imdb.rating" } } },
-  { $project: { _id: "$_id", numeroFilmes: 1, mediaIMDB: { $round: ["$mediaIMDB", 1] } } },
+  { $project: { _id: 1, numeroFilmes: 1, mediaIMDB: { $round: ["$mediaIMDB", 1] } } },
   { $sort: { numeroFilmes: -1, _id: -1 } },
 ]);

1a1eb7f2d4445b5d3aee0279fe5ad1dd31d6b11c
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-03-23 15:18:10 -0300

    desafio 5 tentativa, baseado na resposta de Carlos Souza

---
+++ challenges/desafio5.js
@@ -0,0 +1,20 @@
+db.movies.aggregate([
+  {
+    $match: {
+      $and: [
+        { countries: "USA" },
+        { "tomatoes.viewer.rating": { $gte: 3 } },
+        { cast: { $in: ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"] } },
+      ],
+    },
+  },
+  {
+    $addFields: { num_favs: { $setIntersection: ["$cast", ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"]] } },
+  },
+  {
+    $addFields: { num_favs: { $size: "$num_favs" } },
+  },
+  { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
+  { $project: { title: 1, _id: 0, num_favs: 1 } },
+  { $limit: 90 },
+]);

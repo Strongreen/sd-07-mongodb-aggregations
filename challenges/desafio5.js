702803f616b74c5fddad4ca38ff7182cabf56f41
PedroMarqDev <PedroMarqDev@gmail.com>
2021-03-24 13:47:31 -0300

    Challenges 1-5

---
+++ challenges/desafio5.js
@@ -0,0 +1,17 @@
+db.movies.aggregate([
+  { $match: {
+    countries: "USA",
+    "tomatoes.viewer.rating": { $gte: 3 },
+  } }, { $addFields: {
+    num_favs: {
+      $let: {
+        vars: {
+          actors: ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"],
+        },
+        in: { $setIntersection: ["$$actors", "$cast"] },
+      },
+    },
+  } },
+  { $match: { num_favs: { $ne: null } } },
+  { $addFields: { num_favs: { $size: "$num_favs" } } },
+  { $project: { _id: 0, title: 1 } }, { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } }, { $skip: 24 }, { $limit: 1 }]);

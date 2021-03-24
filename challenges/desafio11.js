90e9304183e35e2819f18272e54dc17e60d4ec16
Vinicius <vini@pop-os.localdomain>
2021-03-24 19:44:37 -0300

    Requisitos 9, 10, 11 e 12

---
+++ challenges/desafio11.js
@@ -0,0 +1,13 @@
+db.trips.aggregate([
+  { $addFields: {
+    weekDay: { $dayOfWeek: "$startTime" },
+  } },
+  { $group: { _id: "$weekDay", total: { $sum: 1 } } },
+  { $project: {
+    _id: 0,
+    diaDaSemana: "$_id",
+    total: 1,
+  } },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+]);

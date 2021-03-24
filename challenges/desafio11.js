f682b76c61d1a3cdab247722b66b38e613e69d78
Vinicius <vini@pop-os.localdomain>
2021-03-24 20:06:27 -0300

    Correção Requisito 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -2,11 +2,11 @@ db.trips.aggregate([
   { $addFields: {
     weekDay: { $dayOfWeek: "$startTime" },
   } },
-  { $group: { _id: "$weekDay", total: { $sum: 1 } } },
+  { $group: { _id: "$weekDay", count: { $sum: 1 } } },
   { $project: {
     _id: 0,
     diaDaSemana: "$_id",
-    total: 1,
+    total: "$count",
   } },
   { $sort: { total: -1 } },
   { $limit: 1 },

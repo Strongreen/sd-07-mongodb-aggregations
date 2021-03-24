a98ff8ad972a22a8c1ad18ed4b3c8ab3f0375062
Bruno <bruno_grb@hotmail.com>
2021-03-24 18:42:17 -0300

    corrigindo lint desafio 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -2,15 +2,15 @@ db.trips.aggregate([
   {
     $group: {
       _id: "$usertype",
-      duracaoMedia: { $avg: { $subtract: ["$stopTime", "$startTime"] }},
+      duracaoMedia: { $avg: { $subtract: ["$stopTime", "$startTime"] } },
     },
   },
   {
     $project: {
       _id: 0,
       tipo: "$_id",
-      duracaoMedia: { $round: [{ $divide: ["$duracaoMedia", 3600000]}, 2]},
-    }
+      duracaoMedia: { $round: [{ $divide: ["$duracaoMedia", 3600000] }, 2] },
+    },
   },
   {
     $sort: {

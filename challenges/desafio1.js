4cbc8947e5aabab54409b4b84d048ca9b45c6c0e
Danwhat <daanwhant@gmail.com>
2021-03-24 16:20:17 -0300

    hate lint

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,9 +1,11 @@
-use('aggregations');
+use("aggregations");
 db.movies.aggregate([{
   $match: {
-    'imdb.rating': {$gte: 7},
-    genres: {$nin: ['Horror', 'Crime']},
-    $or: [{rated: 'PG'},{rated: 'G'}],
+    "imdb.rating": { $gte: 7 },
+    genres: { $nin: ["Horror", "Crime"] },
+    $or: [{ rated: "PG" }, { rated: "G" }],
     languages: { $all: ["English", "Spanish"] },
   }
-}]);
+},
+{ $count: "total" }
+]);

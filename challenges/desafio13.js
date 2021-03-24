29bff7237d392817733de36f9b3dfd7e315f7c5b
samantabelow <samanta.below@gmail.com>
2021-03-24 13:37:50 -0300

    Lint corrigido

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -15,7 +15,7 @@ db.trips.aggregate([
   {
     $project: {
       _id: 0,
-      duracaoMediaEmMinutos: { $ceil: { $divide: [ "$duracaoMedia", 60000]}},
+      duracaoMediaEmMinutos: { $ceil: { $divide: ["$duracaoMedia", 60000] } },
     },
   },
   { $sort: { total: -1 } },

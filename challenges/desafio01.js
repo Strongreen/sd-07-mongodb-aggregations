4c441d43a79b171bca4861c67fb222dd5b73f355
Danwhat <daanwhant@gmail.com>
2021-03-24 16:16:57 -0300

    desafio1

--- challenges/desafio01.js
+++
@@ -1,9 +0,0 @@
-use('aggregations');
-db.movies.aggregate([{
-  $match: {
-    'imdb.rating': {$gte: 7},
-    genres: {$nin: ['Horror', 'Crime']},
-    $or: [{rated: 'PG'},{rated: 'G'}],
-    languages: { $all: ["English", "Spanish"] },
-  }
-}]);

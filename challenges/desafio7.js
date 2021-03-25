44cd7634216612e0bd03f25b4908494760e5987d
Filipi Firmino <contatoic3web@gmail.com>
2021-03-24 22:48:37 -0300

    desafio 7

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -11,7 +11,7 @@ db.movies.aggregate([
   {
     $group: {
       _id: "$cast",
-      mediaIMDB: { $avg: "imdb.rating" },
+      mediaIMDB: { $avg: "$imdb.rating" },
       numeroFilmes: { $sum: 1 },
     },
   },

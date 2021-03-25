a520dec046641bbd2f1fa563ca4240ed713ff783
dennerpl <denner.souzapl@gmail.com>
2021-03-25 13:24:32 -0300

    requisito 9

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -20,4 +20,11 @@ db.trips.aggregate([
       menorAnoNascimento: { $min: { $toInt: "$birthYear" } },
     },
   },
+  {
+    $project: {
+      _id: 0,
+      maiorAnoNascimento: 1,
+      menorAnoNascimento: 1,
+    },
+  },
 ]);

f6f1b7514b1b3c161f077e808bd8d5d8245ef974
PedroMarqDev <PedroMarqDev@gmail.com>
2021-03-26 10:54:13 -0300

    12, 14 dix

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,6 +1,8 @@
 db.trips.aggregate([
-  { $group: { _id: { $dayOfWeek: "$startTime" }, total: { $sum: "$endStationName" } } },
+  { $addFields: { dayOfWeek: { $dayOfWeek: "$startTime" } } },
+  { $match: { dayOfWeek: 5 } },
+  { $group: { _id: "$startStationName", total: { $sum: 1 } } },
   { $sort: { total: -1 } },
-  { $project: { _id: 0, diaDaSemana: "$_id", dia: "$endStationName", total: "$total" } },
-  { $limit: 5 },
+  { $project: { _id: 0, nomeEstacao: "$_id", total: "$total" } },
+  { $limit: 1 },
 ]);

04b52e5e948f9ec5f2df52433830500c2b711b90
Renato Silva <renatosdev@gmail.com>
2021-03-26 10:11:21 -0300

    desafio14

---
+++ challenges/desafio14.js
@@ -0,0 +1,35 @@
+use("aggregations");
+const convertMillisegundosMinutos = 1 * 60 * 1000;
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$bikeid",
+      tempoMedio: {
+        $avg: {
+          $subtract: [
+            "$stopTime", "$startTime",
+          ],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      bikeId: "$_id",
+      duracaoMedia: {
+        $ceil: {
+          $divide: [
+            "$tempoMedio", convertMillisegundosMinutos,
+          ],
+        },
+      },
+    },
+  },
+  {
+    $sort: { duracaoMedia: -1 },
+  },
+  {
+    $limit: 5,
+  },
+]);

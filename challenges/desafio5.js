a580317fbae09645dd5de5b4cd30b13280e21567
Danwhat <daanwhat@gmail.com>
2021-03-27 18:58:53 -0300

    hate lint

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -11,16 +11,16 @@ db.movies.aggregate([
       cast: { $in: atoresFavoritos },
       countries: { $in: ["USA"] },
       "tomatoes.viewer.rating": { $gte: 3 },
-    }
+    },
   },
   {
     $addFields: {
       num_favs: {
-        $size: { $setIntersection: ["$cast", atoresFavoritos] }
+        $size: { $setIntersection: ["$cast", atoresFavoritos] },
       },
-    }
+    },
   },
-  { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1, } },
+  { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
   { $project: { _id: 0, title: 1 } },
   { $skip: 24 },
   { $limit: 1 },

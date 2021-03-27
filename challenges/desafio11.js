f8e1db5f89a8424ac885f551023f0ec2ff87915c
Macarena Rodriguez <macarenar697@gmail.com>
2021-03-27 17:06:19 -0300

    req 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -2,7 +2,6 @@ db.trips.aggregate([
   {
     $group: {
       _id: { $dayOfWeek: "$startTime" },
-      diaDaSemana: "$_id",
       total: { $sum: 1 },
     },
   },
@@ -10,8 +9,8 @@ db.trips.aggregate([
   {
     $project: {
       _id: 0,
-      diaDaSemana: 1,
-      total: 1,
+      diaDaSemana: "$_id",
+      total: "$total",
     },
   },
 

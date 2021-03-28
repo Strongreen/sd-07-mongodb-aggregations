6e48120e3fa563bc6570e613c9b1e4f1e98157a4
paula-jfe <paula.jfe@gmail.com>
2021-03-28 11:16:04 -0300

    5 a 9

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -7,18 +7,22 @@ const favorites = [
 ];
 
 db.movies.aggregate([
-  { $match: {
-    countries: "USA",
-    "tomatoes.viewer.rating": { $gte: 3 },
-    cast: { $exists: true },
-  } },
+  {
+    $match: {
+      countries: "USA",
+      "tomatoes.viewer.rating": { $gte: 3 },
+      cast: { $exists: true },
+    },
+  },
   {
     $addFields: {
       num_favs: { $size: { $setIntersection: [favorites, "$cast"] } },
     } },
-  { $sort: {
-    num_favs: -1, "tomatoes.viewer.rating": -1, title: -1,
-  } },
+  {
+    $sort: {
+      num_favs: -1, "tomatoes.viewer.rating": -1, title: -1,
+    },
+  },
   { $skip: 24 },
   { $limit: 1 },
   { $project: { title: 1, _id: 0 } },

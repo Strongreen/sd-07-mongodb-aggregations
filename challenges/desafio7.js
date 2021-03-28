9c0b625532cf48344d623d8ecabff3af8c5f9aa6
BrunoNogueiraBruno <nugobruno@gmail.com>
2021-03-28 01:13:21 -0300

    Desafio 6

---
+++ challenges/desafio7.js
@@ -0,0 +1,13 @@
+// db.movies.aggregate([
+//   {
+//     $match: {
+//       languages: { $in: ["English"] },
+//     },
+//   },
+//   {
+//     $group: {
+//       _id: null,
+
+//     },
+//   },
+// ]);

2c4d441c8b1f8dd88782681e30b2e3e669dddcf6
Fabio Camargos <fabiocmmcm@gmail.com>
2021-03-29 19:14:07 -0300

    Req 3 - 6

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -9,12 +9,19 @@ db.movies.aggregate([
   },
   {
     $project: {
-      _id: 0,
       titulo: "$title",
       avaliado: "$rated",
       notaIMDB: "$imdb.rating",
       votosIMDB: "$imdb.votes",
       ano: "$year",
+      _id: 0,
+    },
+  },
+  {
+    $sort: {
+      ano: -1,
+      notaIMDB: -1,
+      titulo: 1,
     },
   },
 ]);

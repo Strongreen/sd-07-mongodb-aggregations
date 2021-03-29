3ec2f0ea2bf19826db8ab738105eede7dc1bf021
Camila Valentim <camilaifpb@hotmail.com>
2021-03-29 18:10:56 -0300

    requisitos 9,13 e 14

---
+++ challenges/desafio9.js
@@ -0,0 +1,23 @@
+db.trips.aggregate(
+  [
+    {
+      $match: {
+        birthYear: { $ne: "", $exists: true },
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        maiorAnoNascimento: { $max: "$birthYearInt" },
+        menorAnoNascimento: { $min: "$birthYearInt" },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        maiorAnoNascimento: 1,
+        menorAnoNascimento: 1,
+      },
+    },
+  ],
+);

dbb296eb1e59fd4750c9fd527c068ecab7cb30fc
muriloPereira006 <muriloedu006@gmail.com>
2021-03-29 00:10:38 -0300

    desafio 8 e 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,33 +1,27 @@
 db.trips.aggregate([
   {
-    $match: {
-      usertype: { $exists: true, $not: "" },
-    },
-  },
-  {
     $group: {
       _id: "$usertype",
-      duracao_media: { $avg: { $subtract: ["$stopTime", "$startTime"] } },
+      media: { $avg: { $subtract: ["$stopTime", "$startTime"] } },
     },
-  },
-  {
-    $group: {
-      _id: {
-        tipo: "$_id",
-        duracao_media_em_horas: { $divide: ["$duracao_media", 3600000] },
-      },
-    },
-  },
-  {
-    $sort: { duracaoMedia: 1 },
   },
   {
     $project: {
       _id: 0,
       tipo: "$_id",
-      duracaoMedia: {
-        $round: ["$duracaoMedia", 2],
+      media_sem_aredondar: {
+        $divide: ["$media_de_duracao", 3600000],
       },
     },
   },
+  {
+    $project: {
+      _id: 0,
+      tipo: 1,
+      duracaoMedia: {
+        $round: ["$media_sem_aredondar", 2],
+      },
+    },
+  },
+  { $sort: { duracaoMedia: 1 } },
 ]);

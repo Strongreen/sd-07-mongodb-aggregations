a93ed5e955efa83765415d45c56e07efdaca068e
paula-jfe <paula.jfe@gmail.com>
2021-04-01 21:45:27 -0300

    13 ok

---
+++ challenges/desafio13.js
@@ -0,0 +1,33 @@
+use("aggregations");
+
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: {
+        $gte: ISODate("2016-03-10"),
+        $lt: ISODate("2016-03-11"),
+      },
+    },
+  },
+  {
+    $addFields: {
+      duration: {
+        $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 1000 * 60],
+      },
+    },
+  },
+  {
+    $group: {
+      durationAvg: {
+        $avg: "$duration",
+      },
+      _id: null,
+    },
+  },
+  {
+    $project: {
+      duracaoMediaEmMinutos: { $ceil: "$durationAvg" },
+      _id: 0,
+    },
+  },
+]);

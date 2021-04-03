0f88e9236ee77f5ed12473dcdcb4eddadd195e49
Lucas <lafdesouza2002@outlook.com>
2021-04-03 03:37:15 -0300

    Ultima correçao agora vai

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -9,5 +9,5 @@ db.trips.aggregate([
   },
   { $sort: { total: -1 } },
   { $limit: 1 },
-  { $project: { _id: 0, nomeEstacao: "$_id", total: 1 } },
+  { $project: { _id: 0, nomeEstacao: "$_id", total: "$total" } },
 ]);

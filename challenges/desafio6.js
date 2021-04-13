d7f0c1fe487f3af70d4e6e7f96af2e6f02ffaf6d
Eduardo Pizorno <pizorno@gmail.com>
2021-04-13 20:33:24 -0300

    Desafio 6 fix more issues

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,13 +1,13 @@
 db.movies.aggregate([
   {
     $match: { awards: { $regex: /won(\s\d\s)oscar/gi } } },
-  { $group: { 
+  { $group: {
     _id: null,
     maior_rating: { $max: "$imdb.rating" },
     menor_rating: { $min: "$imdb.rating" },
     media_rating: { $avg: "$imdb.rating" },
     desvio_padrao: { $stdDevSamp: "$imdb.rating" } } },
-  { $project: { 
+  { $project: {
     _id: 0,
     maior_rating: { $round: ["$maior_rating", 1] },
     menor_rating: { $round: ["$menor_rating", 1] },

6c8ff838778f91000583eb364b4ac00e5bf7849c
Eduardo Pizorno <pizorno@gmail.com>
2021-04-13 20:17:02 -0300

    Desafio 5 fix issue

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -11,5 +11,5 @@ db.movies.aggregate([
   { $addFields: { num_favs: { $size: { $setIntersection: [actors, "$cast"] } } } },
   { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
   { $skip: 24 }, { $limit: 1 },
-  { $project: { _id: 0, title: 1 } }
+  { $project: { _id: 0, title: 1 } },
 ]);

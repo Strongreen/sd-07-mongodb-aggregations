ed2cbb2a06079d3b1b92f5cc28915a187bb1b71d
lucasvr39 <lucasveras1994@gmail.com>
2021-04-20 14:00:58 -0300

    try to fix challenge 1 again

--- desafio1.js
+++ desafio1.js
@@ -1,4 +1,18 @@
-const challengeOne = [
+// const challengeOne = [
+//   {
+//     $match: {
+//       $and: [
+//         { "imdb.rating": { $gte: 7 } },
+//         { genres: { $nin: ["Crime", "Horror"] } },
+//         { rated: { $in: ["PG", "G"] } },
+//         { languages: "English" },
+//         { languages: "Spanish" },
+//       ],
+//     },
+//   },
+// ];
+
+db.movies.aggregate([
   {
     $match: {
       $and: [
@@ -10,6 +24,4 @@ const challengeOne = [
       ],
     },
   },
-];
-
-db.movies.aggregate(challengeOne);
+]);

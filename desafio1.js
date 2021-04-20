07f6f91e0268dfcf896535d6fcc21ee759a2003f
lucasvr39 <lucasveras1994@gmail.com>
2021-04-20 14:14:22 -0300

    last try

--- desafio1.js
+++ desafio1.js
@@ -1,26 +1,9 @@
-// const challengeOne = [
-//   {
-//     $match: {
-//       $and: [
-//         { "imdb.rating": { $gte: 7 } },
-//         { genres: { $nin: ["Crime", "Horror"] } },
-//         { rated: { $in: ["PG", "G"] } },
-//         { languages: "English" },
-//         { languages: "Spanish" },
-//       ],
-//     },
-//   },
-// ];
-
 db.movies.aggregate([
   {
     $match: {
       $and: [
-        { "imdb.rating": { $gte: 7 } },
-        { genres: { $nin: ["Crime", "Horror"] } },
-        { rated: { $in: ["PG", "G"] } },
-        { languages: "English" },
-        { languages: "Spanish" },
+        { "imdb.rating": { $gte: 7 } }, { genres: { $nin: ["Crime", "Horror"] } },
+        { rated: { $in: ["PG", "G"] } }, { languages: "English" }, { languages: "Spanish" },
       ],
     },
   },

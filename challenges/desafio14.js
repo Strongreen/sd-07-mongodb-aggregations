b1dad24a75ae153e8602ce7e3f31af7cf3a5443e
Gustavo Saideles da Silveira <saideles.silveira@gmail.com>
2021-05-05 10:20:24 -0300

    varias variaveis variantes que variam

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,24 +1,28 @@
 db.trips.aggregate([
   {
     $addFields: {
-      duration: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60000] },
+      duration: {
+        $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 1000 * 60],
+      },
     },
   },
   {
     $group: {
       _id: "$bikeid",
-      durationAvg: { $avg: "$duration" },
+      durationAvg: {
+        $avg: "$duration",
+      },
     },
   },
   {
     $project: {
       bikeId: "$_id",
-      averageDuration: { $ceil: "$durationAvg" },
+      duracaoMedia: { $ceil: "$durationAvg" },
       _id: 0,
     },
   },
   {
-    $sort: { averageDuration: -1 },
+    $sort: { duracaoMedia: -1 },
   },
   {
     $limit: 5,

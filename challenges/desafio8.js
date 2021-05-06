cbba9fd5286e5285e049ac9c75f85abe3f37e59b
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2021-05-06 01:09:22 -0300

    Esse desafio 8 e 9 ... partiram para ignorância

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,14 +1,36 @@
-db.air_airlines.findOne()
-db.air_alliances.findOne()
-db.air_routes.findOne()
-db.trips.findOne()
+// -------------------------
+// sempre tenha esses caras aqui para ajudar na visualização
+// db.air_airlines.findOne()
+// db.air_alliances.findOne()
+// db.air_routes.findOne()
+// db.trips.findOne()
 
-db.air_routes.find(
-  {airplane: "747"}
-);
+// Olhe sempre no trybe cheat sheets e vá montando os bloquinhos
 
 db.air_routes.aggregate([
   {$match: {
     airplane: {$in: ["747", "380"] }
-  }}
+  }},
+  {
+    $lookup: {
+      from: "air_alliances",
+      localField: "airline.name",
+      foreignField: "airlines",
+      as: "alliance"
+    }
+  },
+  // {$limit: 2}, // resolva o problema para poucos para depois ver como avançar
+  {
+    $group: {
+      _id: "$alliance.name",
+      totalRotas: { $sum: 1},
+    },
+  },
+  {$match: {
+    _id: {$not: {$eq: []}}
+    // _id: {$exists: 1} // seria mais fácil
+  }},
+  {$sort: {totalRotas: -1}},
+  {$limit: 1},
+  {$unwind: "$_id"},
 ])

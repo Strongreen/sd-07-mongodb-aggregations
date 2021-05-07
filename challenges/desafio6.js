493916f6ff6045e8bf344ffa80c202bdc497eb10
lucasvr39 <lucasveras1994@gmail.com>
2021-05-07 12:37:21 -0300

    fix typo errors

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -4,7 +4,7 @@ const regularOscar = /Oscar/i;
 db.movies.aggregate([
   {
     $match: {
-      and: [
+      $and: [
         { awards: { $regex: regularWon } },
         { awards: { $regex: regularOscar } },
       ],
@@ -12,20 +12,20 @@ db.movies.aggregate([
   },
   {
     $group: {
+      _id: null,
       maior_rating: { $max: "$imdb.rating" },
       menor_rating: { $min: "$imdb.rating" },
-      mmedia_rating: { $avg: "$imdb.rating" },
+      media_rating: { $avg: "$imdb.rating" },
       desvio_padrao: { $stdDevSamp: "$imdb.rating" },
-      _id: null,
     },
   },
   {
     $project: {
+      _id: 0,
       maior_rating: 1,
       menor_rating: 1,
       media_rating: { $round: ["$media_rating", 1] },
       desvio_padrao: { $round: ["$desvio_padrao", 1] },
-      _id: 0,
     },
   },
 ]);

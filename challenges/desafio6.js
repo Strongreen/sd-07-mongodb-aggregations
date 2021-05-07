dae3b4a5d705e84bc38ffb6f4e61167296508aa0
lucasvr39 <lucasveras1994@gmail.com>
2021-05-07 12:11:47 -0300

    fix challenge 6 by changing Won regular expression

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,28 +1,31 @@
+const regularWon = /^Won/i;
+const regularOscar = /Oscar/i;
+
 db.movies.aggregate([
   {
     $match: {
       and: [
-        { awards: { $regex: /^Won/i } },
-        { awards: { $regex: /^Oscar/i } },
+        { awards: { $regex: regularWon } },
+        { awards: { $regex: regularOscar } },
       ],
     },
   },
   {
     $group: {
-      _id: null,
       maior_rating: { $max: "$imdb.rating" },
       menor_rating: { $min: "$imdb.rating" },
       mmedia_rating: { $avg: "$imdb.rating" },
       desvio_padrao: { $stdDevSamp: "$imdb.rating" },
+      _id: null,
     },
   },
   {
     $project: {
-      _id: 0,
       maior_rating: 1,
       menor_rating: 1,
       media_rating: { $round: ["$media_rating", 1] },
       desvio_padrao: { $round: ["$desvio_padrao", 1] },
+      _id: 0,
     },
   },
 ]);
